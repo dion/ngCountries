a600af026d5c460aacf840fffa778ec1d4076eb2
Dion Yang <jellopops@gmail.com>
2015-07-21 16:23:38 -0700

    added routing

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-angular.module('countries', ['ngRoute', 'ngAnimate']);
+angular.module('countries', ['ngRoute', 'ngAnimate'])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/home', {
+			templateUrl: 'home.html'
+		})
+		.when('/countries', {
+			templateUrl: 'countries.html'
+		})
+		.otherwise('/home');
+	}]);
